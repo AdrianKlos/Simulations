2a66c8cb6be94d600f712fd5acca5a2a5fae5f06
Adrian Klos <adrianklos2525@gmail.com>
2025-03-26 13:28:48 -0500

    Add files via upload

---
+++ Shatzee/main.py
@@ -0,0 +1,56 @@
+import random
+ticket = 30
+money = 0
+sum = 0
+number = 0
+s = 0
+gs = 0
+rounds = 10000
+
+def rollddice():
+    global money, s, gs
+    pdie = random.randrange(1,7)
+    ndie = random.randrange(1,7)
+    print("-----")
+    print(pdie)
+    print(ndie)
+    print("--0--")
+    print(money)
+    if pdie == 6 and ndie == 1:
+        print("Shatzee")
+        s+=1
+    if pdie > ndie:
+        a = money
+        for i in range (abs(pdie-ndie)):
+            money+=rollwdice()
+            print(money)
+        if money==a+30:
+            print("Grand Shatzee!")
+            gs+=1
+    if pdie < ndie:
+        for i in range (abs(pdie-ndie)):
+            money-=rollwdice()
+            print(money)
+    if pdie == ndie:
+        money+=0
+        print(money)
+
+def rollwdice():
+    print("-----")
+    return random.randrange(1,7)
+
+def main():
+    global money, s, gs, ticket, rounds
+    #choice = int(input("Roll Dice (1)"))
+    #if choice == 1:
+    for i in range(rounds):
+        rollddice()
+    print("--00--")
+    print(money/rounds)
+    print(s)
+    print(s/rounds)
+    print(gs)
+    print(gs/rounds)
+
+
+main()
